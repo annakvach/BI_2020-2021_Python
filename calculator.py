44827c97a4c2202e6961e3f2496df5038af0bafa
annakvach <ay.kvach.r@gmail.com>
2020-09-15 11:44:24 +0300

    added a description to the output of the result and a space between the description of the program and itself

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 
 print('Hello! This is a calculator-program. ')
 print('You can calculate the addition, subtraction, multiplication, division for two numbers.')
-
+print('')
 num_1 = float(input('Enter one number: ',))
 operator = input('Enter one operator (+, -, *, /): ')
 num_2 = float(input('Enter one number: ',))
@@ -9,19 +9,19 @@ num_2 = float(input('Enter one number: ',))
 
 if (operator == "+"):
     result = num_1 + num_2
-    print(result)
+    print("Result = ", result)
 elif (operator == "-"):
     result = num_1 - num_2
-    print(result)
+    print("Result = ", result)
 elif (operator == "*"):
     result = num_1 * num_2
-    print(result)
+    print("Result = ", result)
 elif (operator == "/"):
     if (num_2 == 0):
         print('Error! You cannot divide by Zero!')
     else:
         result = num_1 / num_2
-        print(result)
+        print("Result = ", result)
 
 
 
