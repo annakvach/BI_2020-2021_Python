9a346a16370daef62a3897f40cd7bc44470075d7
annakvach <ay.kvach.r@gmail.com>
2020-09-15 20:16:04 +0300

    + chek is anything a number? + defDigit()

--- calculator.py
+++ calculator.py
@@ -1,34 +1,48 @@
 print('Hello! This is a calculator-program. ')
-print('It has two modes - uni for operations (!, ln) and ')
-print('bi for operations (+, -, *, /, ^(it is exponentiation))',)
+print('It has two modes - UNI for operations (!, ln) and ')
+print('BI for operations (+, -, *, /, ^(it is exponentiation))', )
 print()
 
-import math #import math library
+import math  # import math library for 2 "unitary" operations
 
-mode = input('Chose mode (uni/bi): ')
-# uni for "unitary" operations
-if (mode == "uni"):
+
+# for type control
+def isDigit(x):
+    try:
+        float(x)
+        return True
+    except ValueError:
+        return False
+
+
+mode = input('Chose mode (UNI/BI): ')
+# UNI for "unitary" operations
+if (mode == "UNI"):
     print('You can calculate the factorial(!), natural logarithm(ln)  .')
     operator = input('Enter one operator (!, ln): ')
-    num_2 = float(input('Enter number: ',))
+    input_1 = input('Enter number: ', )  # input anything
 
-    if (operator == "!"):
-        print("Result = ", math.gamma(num_2 + 1))
-    if (operator == "ln"):
-        if (num_2 > 0):
-            print("Result = ", math.log(num_2))
+    if (isDigit(input_1)):  # chek is "anything" a number?
+        num_1 = float(input_1)
+        if (operator == "!"):
+            print("Result = ", math.gamma(num_1 + 1))
+        elif (operator == "ln"):
+            if (num_1 > 0):
+                print("Result = ", math.log(num_1))
+            else:
+                print("Logarithm not defined. Number should be > 0")
         else:
-            print("Logarithm not defined. Number should be > 0")
+            print("Sorry! Invalid operator")
+    else:
+        print("Sorry! Invalid type of number. Try again with number.")
 
-
-
-#bi for "binary" operations
-elif (mode == "bi"):
-    input_1 = input('Enter first number: ',)
+# BI for "binary" operations
+elif (mode == "BI"):
+    input_1 = input('Enter first number: ', )  # input anything
     operator = input('Enter one operator (+, -, *, /, ^): ')
-    input_2 = input('Enter second number: ',)
+    input_2 = input('Enter second number: ', )  # input anything
 
-    if ((input_1 is float) and (input_2 is float)):
+    if ((isDigit(input_1)) and (isDigit(input_2))):  # chek is "anything" a number?
         num_2 = float(input_2)
         num_1 = float(input_1)
 
@@ -53,30 +67,5 @@ elif (mode == "bi"):
     else:
         print("Sorry! Invalid type of numbers (one or both). Try again with two numbers.")
 
-#bi for binary operations
-elif (mode == "bi"):
-    num_1 = float(input('Enter first number: ',))
-    operator = input('Enter one operator (+, -, *, /, **): ')
-    num_2 = float(input('Enter second number: ',))
-
-    if (operator == "+"):
-        result = num_1 + num_2
-        print("Result = ", result)
-    elif (operator == "-"):
-        result = num_1 - num_2
-        print("Result = ", result)
-    elif (operator == "*"):
-        result = num_1 * num_2
-        print("Result = ", result)
-    elif (operator == "/"):
-        if (num_2 == 0):
-            print('Error! You cannot divide by Zero!')
-        else:
-            result = num_1 / num_2
-            print("Result = ", result)
-    elif (operator == "**"):
-        result = num_1 ** num_2
-        print("Result = ", result)
-
 else:
-    print('Sorry! Invalid mode. Try again - uni or bi.')
+    print('Sorry! Invalid mode. Try again - UNI or BI.')
