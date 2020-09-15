a93dc2933385b84b68ce546c92b695dce78f419b
annakvach <ay.kvach.r@gmail.com>
2020-09-15 15:58:57 +0300

    edit factorial for float using math library

--- calculator.py
+++ calculator.py
@@ -2,6 +2,8 @@ print('Hello! This is a calculator-program. ')
 print('It has two modes - uni for unitary operations and bi for binary operations ',)
 print()
 
+import math #import math library
+
 mode = input('Chose mode (uni/bi): ')
 # uni for unitary operations
 if (mode == "uni"):
@@ -10,9 +12,31 @@ if (mode == "uni"):
     num_2 = float(input('Enter number: ',))
 
     if (operator == "!"):
-        result = 1
-        for i in range(2, int(num_2 + 1)):
-            result *= i
+        print("Result = ", math.gamma(num_2 + 1))
+
+#bi for binary operations
+elif (mode == "bi"):
+    num_1 = float(input('Enter first number: ',))
+    operator = input('Enter one operator (+, -, *, /, **): ')
+    num_2 = float(input('Enter second number: ',))
+
+    if (operator == "+"):
+        result = num_1 + num_2
+        print("Result = ", result)
+    elif (operator == "-"):
+        result = num_1 - num_2
+        print("Result = ", result)
+    elif (operator == "*"):
+        result = num_1 * num_2
+        print("Result = ", result)
+    elif (operator == "/"):
+        if (num_2 == 0):
+            print('Error! You cannot divide by Zero!')
+        else:
+            result = num_1 / num_2
+            print("Result = ", result)
+    elif (operator == "**"):
+        result = num_1 ** num_2
         print("Result = ", result)
 
 #bi for binary operations
@@ -39,16 +63,6 @@ elif (mode == "bi"):
     elif (operator == "**"):
         result = num_1 ** num_2
         print("Result = ", result)
-#ERROR. incorrect mode
+
 else:
-    print('Error. You choose incorrect mode')
-
-
-
-
-
-
-
-
-
-
+    print('invalid first number or second number')
