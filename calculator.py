79ef1f5f769c4d118ff8ccede79119b3cfbdf24c
annakvach <ay.kvach.r@gmail.com>
2020-09-15 11:35:23 +0300

    add basic calculator

---
+++ calculator.py
@@ -0,0 +1,27 @@
+
+print('Hello! This is a calculator-program. ')
+print('You can calculate the addition, subtraction, multiplication, division for two numbers.')
+
+num_1 = float(input('Enter one number: ',))
+operator = input('Enter one operator (+, -, *, /): ')
+num_2 = float(input('Enter one number: ',))
+
+
+if (operator == "+"):
+    result = num_1 + num_2
+    print(result)
+elif (operator == "-"):
+    result = num_1 - num_2
+    print(result)
+elif (operator == "*"):
+    result = num_1 * num_2
+    print(result)
+elif (operator == "/"):
+    if (num_2 == 0):
+        print('Error! You cannot divide by Zero!')
+    else:
+        result = num_1 / num_2
+        print(result)
+
+
+
