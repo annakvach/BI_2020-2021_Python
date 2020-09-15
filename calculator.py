421c44a5547f73620a1044c6f532a779de737def
annakvach <ay.kvach.r@gmail.com>
2020-09-15 12:23:35 +0300

    added two modes and edit words in discription + add factorial

--- calculator.py
+++ calculator.py
@@ -1,27 +1,51 @@
 
 print('Hello! This is a calculator-program. ')
-print('You can calculate the addition, subtraction, multiplication, division for two numbers.')
-print('')
-num_1 = float(input('Enter one number: ',))
-operator = input('Enter one operator (+, -, *, /): ')
-num_2 = float(input('Enter one number: ',))
+print('It has two modes - uni for unitary operations and bi for binary operations ',)
+print()
+mode = input('Chose mode (uni/bi): ')
 
+import math #import library
 
-if (operator == "+"):
-    result = num_1 + num_2
-    print("Result = ", result)
-elif (operator == "-"):
-    result = num_1 - num_2
-    print("Result = ", result)
-elif (operator == "*"):
-    result = num_1 * num_2
-    print("Result = ", result)
-elif (operator == "/"):
-    if (num_2 == 0):
-        print('Error! You cannot divide by Zero!')
-    else:
-        result = num_1 / num_2
+if (mode == "uni"):
+    print('You can calculate the factorial, .')
+    operator = input('Enter operator (!): ')
+    num_2 = float(input('Enter number: ',))
+
+    if (operator == "!"):
+        print("Result = ", math.factorial(4))
+
+elif (mode == "bi"):
+    num_1 = float(input('Enter first number: ',))
+    operator = input('Enter one operator (+, -, *, /, **, !): ')
+    num_2 = float(input('Enter second number: ',))
+
+    if (operator == "+"):
+        result = num_1 + num_2
         print("Result = ", result)
+    elif (operator == "-"):
+        result = num_1 - num_2
+        print("Result = ", result)
+    elif (operator == "*"):
+        result = num_1 * num_2
+        print("Result = ", result)
+    elif (operator == "/"):
+        if (num_2 == 0):
+            print('Error! You cannot divide by Zero!')
+        else:
+            result = num_1 / num_2
+            print("Result = ", result)
+    elif (operator == "**"):
+        result = num_1 ** num_2
+        print("Result = ", result)
+
+
+
+
+
+
+
+
+
 
 
 
